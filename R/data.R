e422b92c74107161b4094826106960eba7f63ba6
Yarid Monzon Deras <ymonzonderas@smith.edu>
2025-04-14 09:11:48 -0400

    Add xkcd_comics data set and documentation

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' @title Metadata for the first 10 xkcd comics
+#'
+#' @format
+#' \describe{
+#'  \item{month}{Month of the year the comic was published}
+#' }
+#' @source <https://xkcd.com/json.html>
+"xkcd_comics"
