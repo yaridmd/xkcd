553d815715349488ddbcc782be8e2993f1a969a4
Yarid Monzon Deras <ymonzonderas@smith.edu>
2025-04-14 08:49:35 -0400

    Added draw_comic function to display xkcd comic in RStudio window

--- R/xkcd.R
+++ R/xkcd.R
@@ -25,3 +25,34 @@ xkcd <- function(number) {
   x <- jsonlite::read_json(url)
   return(x)
 }
+
+#' @title Plot an xkcd comic strip
+#' @description
+#' `draw_comic()` display an xkcd comic strip as a plot in a graphics device
+#' (e.g.,  the RStudio plot window).
+#' @param x A list object as returned from [xkcd()]
+#' @importFrom tools file_ext
+#' @importFrom httr GET
+#' @importFrom png readPNG
+#' @importFrom jpeg readJPEG
+#' @importFrom graphics plot.new
+#' @importFrom grid grid.raster
+#' @returns A rastergrob grob. See "Details" section of the [grid::grob()] for more information.
+#' @examples
+#' dino_time <- xkcd(15)
+#' draw_comic(dino_time)
+#' @export
+draw_comic <- function(x) {
+
+  img_type <- tools::file_ext(x$img)
+  tmp <- httr::GET(url = x$img)
+
+  if (img_type == "png") {
+    img <- png::readPNG(tmp$content)
+  } else {
+    img <- jpeg::readJPEG(tmp$content)
+  }
+
+  graphics::plot.new()
+  grid::grid.raster(img)
+}
