9be5bc641cd143af5a9c00b079142945fdd8f543
Yarid Monzon Deras <ymonzonderas@smith.edu>
2025-04-14 08:25:03 -0400

    Finished xkcd documentation

--- R/xkcd.R
+++ R/xkcd.R
@@ -1,8 +1,23 @@
 #' @title Use the xkcd API
 #' @description
 #' Given a comic number, the `xkcd()` function calls the xkcd JSON API and returns metadata about the comic in the form of a list object.
-#'
+#' @param number A scalar numeric vector corresponding to an xkcd comic number
 #' @importFrom jsonlite read_json
+#' @return A list of 13 elements
+#' * `month`: A scalar character vector
+#' * `num`: A scalar numeric vector
+#' * `link`: A scalar character vector, may be empty
+#' * `year`: A scalar character vector
+#' * `news`: A scalar character vector, maybe empty
+#' * `safe_title`: A scalar character vector
+#' * `transcript`: A scalar character vector
+#' * `alt`: A scalar character vector
+#' * `img`: A scalar character vector
+#' * `title`: A scalar character vector
+#' * `day`: A scalar character vector
+#' @examples
+#' dino_time <- xkcd(15)
+#' str(dino_time)
 #' @export
 xkcd <- function(number) {
 
